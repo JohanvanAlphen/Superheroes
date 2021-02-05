73f75847ba7497cc7445db61b6e5afbcd2b7260c
Johan van Alphen <johanvanalphen@hotmail.com>
2021-02-05 15:08:30 +0100

    my solutions for the Superheroes assignment

--- script.js
+++ script.js
@@ -1,90 +1,187 @@
-// [].find
-// [].forEach
-// [].some
-// [].every
-// [].includes
 // [].map
 // [].filter
 // [].reduce
 
-// Opdracht A
-// Schrijf een JavaScript functie die een bepaald item weet te vinden en terug geeft op basis van een bepaalde value.
-// Zoek het hele object van Spiderman, gebaseerd op de name "Spiderman".
 const superheroes = [
-    { name: "Batman", alter_ego: "Bruce Wayne" },
-    { name: "Superman", alter_ego: "Clark Kent" },
-    { name: "Spiderman", alter_ego: "Peter Parker" }]
+    {
+        "name": "Batman",
+        "publisher": "DC Comics",
+        "alter_ego": "Bruce Wayne",
+        "first_appearance": "Detective Comics #27",
+        "weight": "210"
+    },
+    {
+        "name": "Superman",
+        "publisher": "DC Comics",
+        "alter_ego": "Kal-El",
+        "first_appearance": "Action Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Flash",
+        "publisher": "DC Comics",
+        "alter_ego": "Jay Garrick",
+        "first_appearance": "Flash Comics #1",
+        "weight": "195"
+    },
+    {
+        "name": "Green Lantern",
+        "publisher": "DC Comics",
+        "alter_ego": "Alan Scott",
+        "first_appearance": "All-American Comics #16",
+        "weight": "186"
+    },
+    {
+        "name": "Green Arrow",
+        "publisher": "DC Comics",
+        "alter_ego": "Oliver Queen",
+        "first_appearance": "All-American Comics #16",
+        "weight": "195"
+    },
+    {
+        "name": "Wonder Woman",
+        "publisher": "DC Comics",
+        "alter_ego": "Princess Diana",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "165"
+    },
+    {
+        "name": "Blue Beetle",
+        "publisher": "DC Comics",
+        "alter_ego": "Dan Garret",
+        "first_appearance": "Mystery Men Comics #1",
+        "weight": "145"
+    },
+    {
+        "name": "Spider Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Peter Parker",
+        "first_appearance": "Amazing Fantasy #15",
+        "weight": "167"
+    },
+    {
+        "name": "Captain America",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Steve Rogers",
+        "first_appearance": "Captain America Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Iron Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Tony Stark",
+        "first_appearance": "Tales of Suspense #39",
+        "weight": "250"
+    },
+    {
+        "name": "Thor",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Thor Odinson",
+        "first_appearance": "Journey into Myster #83",
+        "weight": "200"
+    },
+    {
+        "name": "Hulk",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Bruce Banner",
+        "first_appearance": "The Incredible Hulk #1",
+        "weight": "1400"
+    },
+    {
+        "name": "Wolverine",
+        "publisher": "Marvel Comics",
+        "alter_ego": "James Howlett",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "200"
+    },
+    {
+        "name": "Daredevil",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Matthew Michael Murdock",
+        "first_appearance": "Daredevil #1",
+        "weight": "200"
+    },
+    {
+        "name": "Silver Surfer",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Norrin Radd",
+        "first_appearance": "The Fantastic Four #48",
+        "weight": "unknown"
+    }
+]
+// Opdracht 1; Maak een array van alle superhelden namen
 
-// Hier komt jouw functie
+superheroes.map((hero) => {
+    console.log(hero.name);
+})
 
-const findSpiderMan = (array) => array.find(superhero => superhero.name === "Spiderman");
-console.log(findSpiderMan(superheroes))
-// Find Spiderman
-// result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
+// Opdracht 2; Maak een array van alle "lichte" superhelden (< 190 pounds)
 
-// Opdracht B  
-// Zorg dat je een array returned met de integers verdubbeld. 
-//Gebruik de .forEach method of, een level hoger: de .map method.  
+const superheroLight = superheroes.filter((hero) => {
+    return hero.weight <= 190;
+})
+console.log("Superheroes die minder wegen dan 190kg", superheroLight)
 
-// .forEach function
-// const doubleArrayValues = [1, 2, 3];
-// doubleArrayValues.forEach(function (value, index, array) {
-//     array[index] = value * 2;
-// });
+// Opdracht 3; Maak een array met de namen van de superhelden die 200 pounds wegen
 
-// .map function
-const numbers = [1, 2, 3];
-const doubleArrayValues = numbers.map(x => x * 2);
+const superhero200 = superheroes.filter((hero) => {
+    return hero.weight == 200;
+})
+    // Chain
+    .map((hero) => {
+        return hero.name
+    })
+console.log("Superheroes die precies 200kg wegen", superhero200)
 
-console.log(doubleArrayValues)
-// result should be [2, 4, 6]
+// Opdracht 4; Maak een array met alle comics waar de superhelden hun "first appearances" hebben gehad
 
-// Opdracht C
-// Schrijf een JavaScript functie die checkt of er een nummer (integer) in de array aanwezig is dat groter is dan 10.
-const numbers1 = [1, 4, 3, 6, 9, 7, 11];
-const numbers2 = [1, 2, 1, 2, 1, 2];
-const containsNumberBiggerThan10 = (array) => array.some(number => number > 10);
+superheroes.map((hero) => {
+    console.log((hero.name), ", 1e optreden in", (hero.first_appearance));
+})
 
-console.log(containsNumberBiggerThan10(numbers1))
-console.log(containsNumberBiggerThan10(numbers2))
-// containsNumberBiggerThan10([1, 4, 3, 6, 9, 7, 11])
-// result should be true
-// containsNumberBiggerThan10([1, 2, 1, 2, 1, 2]})
-// result should be false
+// Opdracht 5; Maak een array met alle superhelden van DC Comics
 
-// Opdracht D
-// Schrijf een JavaScript functie die kijkt of er een bepaalde string: "Italy" in de array aanwezig is.
-const countries = ['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kingdom', 'United States'];
-const isItalyInTheGreat7 = (array) => array.includes('Italy');
-console.log(isItalyInTheGreat7(countries))
-//isItalyInTheGreat7(['Canada', 'France','Germany','Italy','Japan','United Kingdom','United States'])
-// result should be true
+const superheroDC = superheroes.filter((hero) => {
+    return hero.publisher == "DC Comics"
+})
+    .map((hero) => {
+        return hero.name
+    })
+console.log("In DC Comics ", superheroDC)
 
-// Opdracht E
-// Schrijf een JavaScript functie die elke integer vertienvoudigt. 
-// Gebruik .forEach.
+const superheroMarvel = superheroes.filter((hero) => {
+    return hero.publisher == "Marvel Comics"
+})
+    .map((hero) => {
+        return hero.name
+    })
+console.log("In Marvel ", superheroMarvel)
 
-const tenfold = [1, 4, 3, 6, 9, 7, 11];
-tenfold.forEach(function (value, index, array) {
-    array[index] = value * 10;
-});
-console.log(tenfold)
-// result should be [10, 40, 30, 60, 90, 70, 110]
+// Opdracht 6; Tel het gewicht van alle superhelden van DC Comics bij elkaar op
 
-// Opdracht F
-//Schrijf een JavaScript functie die checkt of alle waardes in de array onder de 100 zijn, en true or false retourneert.
+const superheroDCTotalWeight = superheroes.filter((hero) => {
+    return hero.weight !== "unknown" && hero.publisher == "DC Comics"
+}).reduce((currentTotal, hero) => {
+    return parseInt(hero.weight) + currentTotal;
+}, 0);
+console.log("De superhelden van DC Comics wegen bij elkaar ", (superheroDCTotalWeight))
 
-const numbers3 = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98];
-const isBelow100 = (array) => array.some(number => number > 100);
-console.log(isBelow100(numbers3))
-// result should be: false
+// Opdracht 7; Tel het gewicht van alle superhelden van Marvel Comics bij elkaar op
 
-// Opdracht G
-//Schrijf een JavaScript functie die alle waardes in de array bij elkaar optelt en het totaal van de som retourneert. 
-//Gebruik de .reduce method. En echt, gebruik Google.
+const superheroMarvelTotalWeight = superheroes.filter((hero) => {
+    return hero.weight !== "unknown" && hero.publisher == "Marvel Comics"
+}).reduce((currentTotal, hero) => {
+    return parseInt(hero.weight) + currentTotal;
+}, 0);
+console.log("De superhelden van Marvel Comics wegen bij elkaar ", (superheroMarvelTotalWeight))
 
-const bigSum = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206, 234];
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// Opdracht 8; zoek de zwaarste superheld (BONUS)
 
-console.log(bigSum.reduce(reducer));
-// result should be 1118
+const heaviestHero = superheroes.filter((hero) =>
+    (hero.weight !== "unknown")).reduce((highestWeight, hero) => {
+        if (parseInt(hero.weight) > highestWeight) {
+            highestWeight = parseInt(hero.weight);
+        };
+        return highestWeight
+    }, 0);
+console.log("De zwaarste Superhero weegt", heaviestHero, "kg")
